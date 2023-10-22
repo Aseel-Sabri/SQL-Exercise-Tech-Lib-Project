d431ed9f3745a5764bd3ed42570bca474bf5e746
Aseel-Sabri <aseel.k.sabri@gmail.com>
2023-10-22 10:16:00 +0300

    Update indexes

--- Database Setup/create_indexes.sql
+++ Database Setup/create_indexes.sql
@@ -1,24 +1,15 @@
-USE Tech_Lib;
+USE TechLib;
 
-CREATE INDEX IX_Books_Title
-ON Books (Title);
-GO
-
-CREATE INDEX IX_Books_Author
-ON Books (Author);
-GO
-
-CREATE INDEX IX_Borrowers_FirstNameLastName
-ON Borrowers (First_Name, Last_Name);
+CREATE INDEX IX_Loans_DateBorrowed
+ON Loans (DateBorrowed);
 GO
 
 CREATE INDEX IX_Loans_BorrowerID
-ON Loans (Borrower_ID);
+ON Loans (BorrowerID);
 GO
 
 CREATE INDEX IX_Loans_BookID
-ON Loans (Book_ID);
+ON Loans (BookID);
 GO
-
 
 
